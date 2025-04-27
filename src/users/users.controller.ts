55d9b48eaf33193e1dbd25e8856ae8801b9eb80e
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-04-27 15:39:09 +0800

    feat: create user with validations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller()
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  createUser(newUser: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data: newUser });
+  }
+
+  getUser() {}
+
+  getUserById() {}
+}
