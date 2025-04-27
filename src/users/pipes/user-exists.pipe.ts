030f7bd43b9c394ee8bf56f2edc93dbc7bc2389c
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-04-27 19:40:32 +0800

    chore: pipe documentation

--- src/users/pipes/user-exists.pipe.ts
+++ src/users/pipes/user-exists.pipe.ts
@@ -11,6 +11,13 @@ import { UsersService } from '../users.service';
 export class UserExistsPipe implements PipeTransform<number, Promise<User>> {
   constructor(private readonly usersService: UsersService) {}
 
+  /**
+   * Validates if a user exists based on the provided user ID.
+   * @param value - The user ID passed as a parameter.
+   * @param metadata - Metadata about the argument being processed (not used here).
+   * @returns The user object if it exists.
+   * @throws HttpException if the user does not exist.
+   */
   async transform(value: number, metadata: ArgumentMetadata): Promise<User> {
     const user = await this.usersService.getUserById(value);
 
